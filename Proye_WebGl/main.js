cce5df1ae89dc1cfde31f83003203d33db33d1fe
scarlett-gm <scarlettmartinez.08893@gmail.com>
2025-04-21 14:23:24 -0600

    actualizacion 3

--- Proye_WebGl/main.js
+++ Proye_WebGl/main.js
@@ -1,12 +1,1 @@
 
-  document.getElementById('btn-campo').addEventListener('click', function () {
-    document.getElementById('contenido-dinamico').innerHTML = '<p>Contenido de fuentes de campo magnético.</p>';
-  });
-
-  document.getElementById('btn-tierra').addEventListener('click', function () {
-    document.getElementById('contenido-dinamico').innerHTML = '<p>Contenido sobre la Tierra.</p>';
-  });
-
-  document.getElementById('btn-imanes').addEventListener('click', function () {
-    document.getElementById('contenido-dinamico').innerHTML = '<p>Contenido sobre imanes.</p>';
-  });
